10ec8f1ee1a8e670c44e776af43c3432baf0511d
Sholesi Kofoworola <kofoworola.sholesi@cottacush.com>
2021-01-27 14:39:37 +0100

    sort-list.py

--- Python/sort-list.py
+++ Python/sort-list.py
@@ -5,3 +5,20 @@
 ''' This solution timed out on leet code for a larger input
     because the time complexity for this algorithm is O(n^2)'''
 
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        while True:
+            current = head
+            swap = 0
+            while current.next is not None:
+                if current.val > current.next.val:
+                    temp = current.val
+                    current.val = current.next.val
+                    current.next.val = temp
+                    swap = 1
+                current = current.next
+            if swap == 0:
+                break
+        return head
