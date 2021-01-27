46f5588aa83466db2e7e8681414863aabe1feb5c
Sholesi Kofoworola <kofoworola.sholesi@cottacush.com>
2021-01-27 14:38:23 +0100

    sort-list.py

--- Python/sort-list.py
+++ Python/sort-list.py
@@ -1,3 +1,6 @@
 # https://leetcode.com/problems/sort-list/submissions/
 
 # Solution using bubble sort
+
+''' This solution timed out on leet code for a larger input
+    because the time complexity for this algorithm is O(n^2)'''
